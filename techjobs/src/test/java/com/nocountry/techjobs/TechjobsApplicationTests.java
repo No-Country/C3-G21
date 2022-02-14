8078c43a8ec3bff0a893e5cfeaab0cf3074fe469
LucioGramaccioni <lucio.gramaccioni@gmail.com>
2022-02-14 17:28:47 -0300

    Creamos entorno de trabajo

---
+++ techjobs/src/test/java/com/nocountry/techjobs/TechjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.techjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
