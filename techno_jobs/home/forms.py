9e3999e2143d59641bfceee23f6bd708336d719e
Paola Cartala <paola.cartala@gmail.com>
2022-03-04 18:05:40 -0300

    avanza en models

--- techno_jobs/home/forms.py
+++ techno_jobs/home/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .constants import COUNTRY_CHOICES
+
 class UserRegisterForm(UserCreationForm):
     # FIXME: buscar como agregar un checkbox para definir el user, si es perfil o compañia
 
@@ -9,8 +11,8 @@ class UserRegisterForm(UserCreationForm):
     # help_text es lo que aparece en el input
     # widget será cómo aparecerá en el HTML (con *****)
     # las variables tienen que ser password1 y password2 porque así lo determina django para la pass y la confirmacion
-    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput, max_length=20)
-    password2 = forms.CharField(label='Confirma tu contraseña', widget=forms.PasswordInput, max_length=20)
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput, max_length=20, required=True)
+    password2 = forms.CharField(label='Confirma tu contraseña', widget=forms.PasswordInput, max_length=20, required=True)
 
     class Meta:
         """
@@ -21,3 +23,14 @@ class UserRegisterForm(UserCreationForm):
         help_texts = { p: '' for p in fields }
 
 # TODO: UserProfileForm con el perfil completo del user
+class UserProfileForm(forms.Form):
+    name = forms.CharField(max_length=30, required=True)
+    lastname = forms.CharField(max_length=30, required=True)
+    # profile_pic = forms
+    cv = forms.FileField(required=True)
+
+class CompanyProfileForm(forms.Form):
+    company_name = forms.CharField(max_length=150, required=True)
+    location = forms.ChoiceField(choices=COUNTRY_CHOICES)
+    phone = forms.IntegerField()
+    web = forms.CharField(max_length=200, required=False)
--- techno_jobs/home/models.py
+++ techno_jobs/home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from .constants import COUNTRY_CHOICES
 
 # Create your models here.
+class Offer(models.Model):
+    title = models.CharField(max_length=150)
 class User(models.Model):
     username = models.CharField(max_length=30)
     email = models.EmailField()
@@ -8,6 +11,22 @@ class User(models.Model):
     password2 = models.CharField(max_length=20)
 
     # TODO: agregar campos del perfil completo de la otra form
+    name = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    job_offer_id = models.ManyToManyField(Offer)
 
     def __str__(self) -> str:
-        return self.username
+        return f'{self.id}: {self.lastname}, {self.name}'
+
+class Company(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    password1 = models.CharField(max_length=20)
+    password2 = models.CharField(max_length=20)
+
+    company_name = models.CharField(max_length=150)
+    location = models.CharField(choices=COUNTRY_CHOICES, max_length=50)
+
+
+    def __str__(self) -> str:
+        return f'{self.id}: {self.lastname}, {self.name}'
