4bf5086681dd9a30705fe36fb58da021b7c4115d
Paola Cartala <paola.cartala@gmail.com>
2022-03-04 15:56:35 -0300

    unifica apps

--- techno_jobs/home/views.py
+++ techno_jobs/home/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+from .models import *
+from .forms import UserRegisterForm
 
 # Create your views here.
 def home(request):
@@ -6,3 +11,36 @@ def home(request):
 
 def faq(request):
     return render(request, 'home/faq.html')
+
+def register(request):
+    if request.method == 'POST':
+        # accedemos a la información que se envió en el form
+        form = UserRegisterForm(request.POST)
+        # verificamos validez de la form (si se llenó correctamente)
+        if form.is_valid():
+            # guardamos
+            form.save()
+            # si la form es válida, accedemos a los datos
+            username = form.cleaned_data['username']
+            print(username)
+
+            # autenticar
+            user = authenticate(username=username, password=form.cleaned_data['password1'])
+            login(request, user)
+
+            #FIXME: mensaje de éxito [toastr]
+            messages.success(request, f'Usuario {username} ha sido creado')
+            return redirect(to='home/user-profile.html')
+    else: 
+        form = UserRegisterForm()
+    context = { 'form': form }
+    return render(request, 'home/register.html', context)
+
+def user_profile(request):
+    return render(request, 'home/user-profile.html')
+
+def logout_view(request):
+    logout(request)
+    # FIXME: toastr
+    messages.success(request, f'Has cerrado sesión correctamente')
+    return redirect(to='home/index.html')
